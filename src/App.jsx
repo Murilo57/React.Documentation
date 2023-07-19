8055832310e60888373d69c1db9ccd9b2aa76d4a
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-07-19 10:28:47 -0300

    added img
    added function for img of perfil

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import './App.css'
 
-<img className="avatar"/>
+const user = {
+  name: 'Murilo Ferreira',
+  imageUrl: 'https://scontent-gru2-2.xx.fbcdn.net/v/t39.30808-6/275993107_149936377502180_1097556940795031548_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=oE9d6CWtqJ8AX92qFQd&_nc_oc=AQmGy0JYwKJXLbS5zKF-YcPpyHbIVc2Af7ZC5W3-0J3EDolCpUzx6Kayi9EdDpc02Gw&_nc_ht=scontent-gru2-2.xx&oh=00_AfAN2mmqnWxBvwW-m2ZZoJ5BdeocT8xk94oNm2jTdZ0bTQ&oe=64BC7535',
+  imageSize: 90,
+}
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img 
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name} 
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+        />
+    </>
+  )
+}
 
 function MyButton() {
   return (
@@ -22,8 +43,10 @@ export default function MyApp(){
   <div>
     <avatar/>
     <AboutPage/> 
+    <Profile/>
     <h1> Welcome to my app </h1>
     <MyButton/>
   </div>
   )
 }
+
